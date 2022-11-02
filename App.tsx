e5500b10fbe5fa59bd49a03a38133b9b393aca8c
Tomas Figueroa <tomy_salta14@hotmail.com>
2022-11-01 23:44:55 -0300

    try firebase

--- App.tsx
+++ App.tsx
@@ -7,22 +7,164 @@ import { Contacto } from './pages/Contacto';
 import { Inicio } from './pages/Inicio';
 import Maps from './pages/Maps';
 import { PuntosTuristicos } from './pages/PuntosTuristicos';
+import { firebaseConfig } from './firebase.config';
+import React, {useEffect} from 'react';
+import { Image, Text, StyleSheet, View, ScrollView, TouchableOpacity, TextInput, Button, Alert } from 'react-native';
+import { BlurView } from 'expo-blur';
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
+import { initializeApp } from 'firebase/app';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { useNavigation } from '@react-navigation/native';
+
+const uri = 'https://ak.picdn.net/shutterstock/videos/1060308725/thumb/1.jpg'
+const profilePicture = 'https://randomuser.me/api/portraits/men/34.jpg'
 
 const Menu = createDrawerNavigator()
 
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+}
+
+function LoginScreen() {
+
+  const [email, setEmail] = React.useState('')
+  const [password, setPassword] = React.useState('')
+  const navigation = useNavigation();
+
+  const app = initializeApp(firebaseConfig);
+  const auth = getAuth(app);
+
+  const handleCreateAccount = () => {
+    createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      console.log('Account created!')
+      const user = userCredential.user;
+      console.log(user)
+    })
+    .catch(error => {
+      console.log(error)
+      Alert.alert(error.message)
+    })
+  }
+
+  const handleSignIn = () => {
+    signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      console.log('Signed in!')
+      const user = userCredential.user;
+      console.log(user)
+      // navigation.navigate("Home");
+    })
+    .catch(error => {
+      console.log(error)
+    })
+  }
+
+
+  return (
+    <View style={styles.container}>
+      <Image source={{ uri }} style={[styles.image, StyleSheet.absoluteFill]} />
+      <View style={{width: 100, height: 100, backgroundColor: 'purple', position: 'absolute' }}></View>
+      <View style={{width: 100, height: 100, backgroundColor: 'blue', top: 120, position: 'absolute', transform: [{rotate: '25deg'}] }}></View>
+      <View style={{width: 100, height: 100, backgroundColor: 'red', bottom: 120 ,position: 'absolute', borderRadius: 50, transform: [{rotate: '50deg'}] }}></View>
+      <ScrollView contentContainerStyle= {{
+        flex: 1,
+        width: '100%',
+        height: '100%',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}> 
+        <BlurView intensity={100}>
+          <View style={styles.login}>
+            <Image source={{ uri: profilePicture }} style={styles.profilePicture} />
+            <View>
+              <Text style={{fontSize: 17, fontWeight: '400', color: 'white'}}>E-mail</Text>
+              <TextInput onChangeText={(text) => setEmail(text)} style={styles.input} placeholder="betomoedano@outlook.com" />
+            </View>
+            <View>
+              <Text style={{fontSize: 17, fontWeight: '400', color: 'white'}}>Password</Text>
+              <TextInput onChangeText={(text) => setPassword(text)} style={styles.input} placeholder="password" secureTextEntry={true}/>
+            </View>
+            <TouchableOpacity onPress={handleSignIn} style={[styles.button, {backgroundColor: '#00CFEB90'}]}>
+              <Text style={{fontSize: 17, fontWeight: '400', color: 'white'}}>Login</Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={handleCreateAccount} style={[styles.button, {backgroundColor: '#6792F090'}]}>
+              <Text style={{fontSize: 17, fontWeight: '400', color: 'white'}}>Create Account</Text>
+            </TouchableOpacity>
+          </View>
+        </BlurView>
+      </ScrollView>
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
  
   return (
-  <NavigationContainer>
-    <Menu.Navigator>
-      <Menu.Screen name='Inicio' component={Inicio}/>
-      <Menu.Screen name='Maps' component={Maps}/>
-      <Menu.Screen name='Categorias' component={Categorias}/>
-      <Menu.Screen name='Contacto' component={Contacto}/>
-      <Menu.Screen name='Puntos Turisticos' component={PuntosTuristicos}/>
-      <Menu.Screen name='Acerca De' component={Acercade}/>
-    </Menu.Navigator>
-  </NavigationContainer> 
+    <NavigationContainer>
+    <Stack.Navigator initialRouteName="Login">
+      <Stack.Screen name="Login" component={LoginScreen} />
+      <Stack.Screen name="Inicio" component={Inicio} />
+    </Stack.Navigator>
+  </NavigationContainer>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+    resizeMode: 'cover',
+  },
+  login: {
+    width: 350,
+    height: 500,
+    borderColor: '#fff',
+    borderWidth: 2,
+    borderRadius: 10,
+    padding: 10,
+    alignItems: 'center',
+  },
+  profilePicture: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+    borderColor: '#fff',
+    borderWidth: 1,
+    marginVertical: 30
+  },
+  input: {
+    width: 250,
+    height: 40,
+    borderColor: '#fff',
+    borderWidth: 2,
+    borderRadius: 10,
+    padding: 10,
+    marginVertical: 10,
+    backgroundColor: '#ffffff90',
+    marginBottom: 20
+  },
+  button: {
+    width: 250,
+    height: 40,
+    borderRadius: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginVertical: 10,
+    borderColor: '#fff',
+    borderWidth: 1,
+  }
+
+});
